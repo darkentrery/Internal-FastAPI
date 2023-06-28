881ab0eaff4be43be43be09e6ff4f58f4019507c
darkentrery <badajkovmihail@gmail.com>
2023-06-28 11:20:10 +0300

    add cli

--- env_fastapi/settings.py
+++ env_fastapi/settings.py
@@ -8,9 +8,9 @@ class Settings(BaseSettings):
     main_url: str
 
 
-dotenv_path = os.path.join(os.path.dirname(__file__), "../.env")
-if os.path.exists(dotenv_path):
-    load_dotenv(dotenv_path)
+# dotenv_path = os.path.join(os.path.dirname(__file__), "../.env")
+# if os.path.exists(dotenv_path):
+#     load_dotenv(dotenv_path)
 
 
 settings = Settings()
